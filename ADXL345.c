f761e0d129215bb6b49f63a0fec00227a3166c70
Derek Thrasher <Thrasher8390@gmail.com>
2015-01-26 22:36:39 -0500

    Feature: Averaging AXL345 readings in a structure Reformatted to 3 spaces for a tab

--- ADXL345.c
+++ ADXL345.c
@@ -7,22 +7,39 @@
 //*****************************************************************************
 //		Includes
 //*****************************************************************************
+
 #include "ADXL345.h"
 #include "I2C.h"
-
 
 //*****************************************************************************
 //		Defines
 //*****************************************************************************
 
+#define	NUMBER_OF_ADXL345_READINGS_TO_AVERAGE     (32)		/*!<Effects AVERAGE_ADXL345_READINGS*/
+#define	AVERAGE_ADXL345_READINGS(x)               ((x>>5))
+
+//*****************************************************************************
+//		Data
+//*****************************************************************************
+
+struct ADXL345_Data AverageReadings;         /*!< This is average every 'NUMBER_OF_ADXL345_READINGS_TO_AVERAGE' Foreground Cycles = 32 * UNDEFINED_FOREGROUND_CYCLE_TIME */
+struct ADXL345_Data InstantaneousReadings; 	/*!< These values are read every Foreground cycle (UNDEFINED_FOREGROUND_CYCLE_TIME) */
+uint8_t DeviceID;
+
+//*****************************************************************************
+//		local Functions
+//*****************************************************************************
+
+static void averageReadings();
+static void printADXL345Readings();
+
 //*****************************************************************************
 //		Global Functions
 //*****************************************************************************
+
 /*!
  * \brief Init I2C
  */
-
-
 void ADXL345_Init()
 {
 	//Lets initialize the I2C module that will be used for the ADXL345
@@ -41,20 +58,87 @@ void ADXL345_Init()
 	I2C_Write0(ADXL345_ADDRESS_0,ADXL345_BW_RATE,ADXL345_BWRATE_100);
 	//Fifo_control
 	I2C_Write0(ADXL345_ADDRESS_0,ADXL345_FIFO_CTL, ADXL345_FIFOCTL_BYPASS);
+
+	//Set Data Format (pg26)
+	//SELF_TEST = 0 / SPI = 0 / INT_INVERT = 0 / 0 / FULL_RES = 0 / Justify = 0 / Range = 00
+	uint8_t dataFormat = 0x00;
+	I2C_Write0(ADXL345_ADDRESS_0,ADXL345_DATA_FORMAT,dataFormat);
+
+	//Init Information
+	DeviceID = I2C_Read0(ADXL345_ADDRESS_0, ADXL345_REG_UID);
+	UARTprintf("ReadValue UID: %d\n",DeviceID);
 }
 
-uint8_t ADXL345_Read()
+/*!
+ * \brief Reads Instantaneous DATA from Accelerometer
+ * \notes Should take place in Foreground which is ran periodically from a Timer!!!
+ */
+void ADXL345_Read()
 {
-	uint8_t readData;
 	//Lets just read the uid at first
-	UARTprintf("ReadValue UID: %d\n",I2C_Read0(ADXL345_ADDRESS_0, ADXL345_REG_UID));;
-	UARTprintf("ReadValueX: %d\n",(((I2C_Read0(ADXL345_ADDRESS_0, DATAX1)&0x03)<<8) + I2C_Read0(ADXL345_ADDRESS_0, DATAX0)));
-	//UARTprintf("ReadValue: %d\n",I2C_Read0(ADXL345_ADDRESS_0, DATAX1));
-	UARTprintf("ReadValueY: %d\n",(((I2C_Read0(ADXL345_ADDRESS_0, DATAY1) & 0x03)<<8)+I2C_Read0(ADXL345_ADDRESS_0, DATAY0)));
-	//UARTprintf("ReadValue: %d\n",I2C_Read0(ADXL345_ADDRESS_0, DATAY1));
-	UARTprintf("ReadValueZ: %d\n",(((I2C_Read0(ADXL345_ADDRESS_0, DATAZ1)&0x03)<<8)+I2C_Read0(ADXL345_ADDRESS_0, DATAZ0)));
-	UARTprintf("\n");
-	//UARTprintf("ReadValue: %d\n",I2C_Read0(ADXL345_ADDRESS_0, DATAZ1));
-	return readData;
+	//TODO Implement multi byte read so data does not change between reads!
+	uint8_t xHigh	= I2C_Read0(ADXL345_ADDRESS_0, DATAX1);
+	uint8_t xLow 	= I2C_Read0(ADXL345_ADDRESS_0, DATAX0);
+	InstantaneousReadings.x = ((xHigh << 8) + xLow);
+
+	uint8_t yHigh	= I2C_Read0(ADXL345_ADDRESS_0, DATAY1);
+	uint8_t yLow 	= I2C_Read0(ADXL345_ADDRESS_0, DATAY0);
+	InstantaneousReadings.y = ((yHigh << 8) + yLow);
+
+	uint8_t zHigh	= I2C_Read0(ADXL345_ADDRESS_0, DATAZ1);
+	uint8_t zLow 	= I2C_Read0(ADXL345_ADDRESS_0, DATAZ0);
+	InstantaneousReadings.z = ((zHigh << 8) + zLow);
+
+	//printADXL345Readings("Instantaneous ADXL345\n", InstantaneousReadings);
+
+	averageReadings();
+}
+
+//*****************************************************************************
+//		Local Functions
+//*****************************************************************************
+
+/*!
+ *\brief This averages the readings over a sent number of readings
+ */
+void averageReadings()
+{
+	static uint8_t numberOfReadingsTaken = 0;
+	static struct ADXL345_Data readingSum = {0,0,0};
+	//Keep summing values until we have enough
+	if(numberOfReadingsTaken <= NUMBER_OF_ADXL345_READINGS_TO_AVERAGE)
+	{
+		readingSum.x += InstantaneousReadings.x;
+		readingSum.y += InstantaneousReadings.y;
+		readingSum.z += InstantaneousReadings.z;
+		numberOfReadingsTaken++;
+	}
+	//Lets Average and reset our averaging varialbes
+	else
+	{
+		AverageReadings.x = AVERAGE_ADXL345_READINGS(readingSum.x);
+		AverageReadings.y = AVERAGE_ADXL345_READINGS(readingSum.y);
+		AverageReadings.z = AVERAGE_ADXL345_READINGS(readingSum.z);
+		numberOfReadingsTaken = 0;
+		readingSum.x = 0;
+		readingSum.y = 0;
+		readingSum.z = 0;
+		printADXL345Readings("Average ADXL345\n", InstantaneousReadings);
+	}
+}
+
+/*!
+ * \brief prints out the x,y,z values in ADXL345_Data format
+ */
+void printADXL345Readings(const char *tag, struct ADXL345_Data data)
+{
+	if(true)
+	{
+		UARTprintf(tag);
+		UARTprintf("ReadValueX: %d\n",data.x);
+		UARTprintf("ReadValueY: %d\n",data.y);
+		UARTprintf("ReadValueZ: %d\n",data.z);
+		UARTprintf("\n");
+	}
 }
 
--- ADXL345.h
+++ ADXL345.h
@@ -12,47 +12,57 @@
 //		Includes
 //*****************************************************************************
 #include "Project.h"
+#include "stdio.h"
 
 
 //*****************************************************************************
 //		Defines
 //*****************************************************************************
-#define ADXL345_ADDRESS_0 		(0x001D)
+#define ADXL345_ADDRESS_0     (0x0053)
 
-#define ADXL345_POWER_CTL  		(0x2D)	//Power Control Register
-#define ADXL345_DATA_FORMAT 	(0x31)
-#define ADXL345_INT_ENABLE 		(0x2E)
-#define ADXL345_FIFO_CTL 		(0x38)
-#define ADXL345_INT_MAP 		(0x2F)
-#define ADXL345_BW_RATE 		(0x2C)
-#define ADXL345_REG_UID			(0x00)
-#define DATAX0  				(0x32)	//X-Axis Data 0
-#define DATAX1  				(0x33)	//X-Axis Data 1
-#define DATAY0  				(0x34)	//Y-Axis Data 0
-#define DATAY1  				(0x35)	//Y-Axis Data 1
-#define DATAZ0  				(0x36)	//Z-Axis Data 0
-#define DATAZ1  				(0x37)	//Z-Axis Data 1
+#define ADXL345_POWER_CTL     (0x2D)	//Power Control Register
+#define ADXL345_DATA_FORMAT   (0x31)
+#define ADXL345_INT_ENABLE    (0x2E)
+#define ADXL345_FIFO_CTL      (0x38)
+#define ADXL345_INT_MAP       (0x2F)
+#define ADXL345_BW_RATE       (0x2C)
+#define ADXL345_REG_UID       (0x00)
+#define ADXL345_DATA_FORMAT   (0x31)
+#define DATAX0                (0x32)	//X-Axis Data 0
+#define DATAX1                (0x33)	//X-Axis Data 1
+#define DATAY0                (0x34)	//Y-Axis Data 0
+#define DATAY1                (0x35)	//Y-Axis Data 1
+#define DATAZ0                (0x36)	//Z-Axis Data 0
+#define DATAZ1                (0x37)	//Z-Axis Data 1
 
 //DataFormat Values
-#define ADXL345_DATAFORMAT_4G 	(0x01)
+#define ADXL345_DATAFORMAT_4G    (0x01)
 
 //Power CTL Values
 #define ADXL345_POWERCTL_UNKNOWN (0x08)
 
 //INT
-#define ADXL345_INTMAP_INT1		(0x7F)
-#define ADXL345_INTEN_ENABLE	(0x08)
+#define ADXL345_INTMAP_INT1      (0x7F)
+#define ADXL345_INTEN_ENABLE     (0x08)
 
 //BW Rate
-#define ADXL345_BWRATE_3200 	(0x0F)
-#define ADXL345_BWRATE_100		(0x0A)
+#define ADXL345_BWRATE_3200      (0x0F)
+#define ADXL345_BWRATE_100       (0x0A)
 //FIFO Controll
-#define ADXL345_FIFOCTL_BYPASS (0x00)
+#define ADXL345_FIFOCTL_BYPASS   (0x00)
+//*****************************************************************************
+//		Global Structs
+//*****************************************************************************
+struct ADXL345_Data
+{
+	int32_t x;
+	int32_t y;
+	int32_t z;
+};
 //*****************************************************************************
 //		Global Functions
 //*****************************************************************************
-extern void 	ADXL345_Init();
-extern uint8_t 	ADXL345_Read();
-
+extern void    ADXL345_Init();
+extern void    ADXL345_Read();
 
 #endif /* ADXL345_H_ */
